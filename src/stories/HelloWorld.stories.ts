d34133f7a4d016a3d133d3c8c2992daf96b059d5
Félix Jullien <jullienfelix@gmail.com>
2022-03-22 15:15:25 +0100

    chore(npm): add yaml-lint dependency

---
+++ src/stories/HelloWorld.stories.ts
@@ -0,0 +1,17 @@
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  title: 'Example/HelloWorld',
+  component: HelloWorld,
+  argTypes: {},
+}
+
+const Template = (args: string) => ({
+  components: { HelloWorld },
+  setup() {
+    return { args }
+  },
+  template: '<hello-world />',
+})
+
+export const Primary = Template.bind({})
