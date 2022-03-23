a376f8799ccde156f3afbf6316701c6b96087938
Félix Jullien <jullienfelix@gmail.com>
2022-03-23 11:43:19 +0100

    chore(storybook): use generated storybook config

--- .storybook/main.ts
+++
@@ -1,8 +0,0 @@
-import viteFinal from './mergeViteConfig'
-export default {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/vue3',
-  core: { builder: 'storybook-builder-vite' },
-  viteFinal,
-}
