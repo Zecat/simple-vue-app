a497b5d5408824edf799360deb4b74f3c0158304
Zecat <jullienfelix@gmail.com>
2022-03-24 15:21:58 +0000

    Deploying to gh-pages from @ Zecat/simple-vue-app@b12800dd0f09f499caf01094845d91611925a0d0 🚀

---
+++ assets/AboutView.4b974309.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a9d0367d.js";import{o as t,c as o,a as _}from"./vendor.2b48ff05.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
